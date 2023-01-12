fd4fee292a78d21d934f1888e2d26d402780030f
Thara-PSU <73512825+Thara-PSU@users.noreply.github.com>
2023-01-12 09:19:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,10 @@ else:
 
 
 # Reads in saved classification model
-load_clf = pickle.load(open('vp_lr_clf.pkl', 'rb'))
+f = open('vp_lr_clf.pkl', 'rb')
+load_clf  = pickle.load(f)
+f.close()
+#load_clf = pickle.load(open('vp_lr_clf.pkl', 'rb'))
  
 
 # Apply model to make predictions
